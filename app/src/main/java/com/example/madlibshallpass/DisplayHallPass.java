35dbeaba91a0deccb850e585181c207384053b77
mrsndr <jason.sndr@gmail.com>
2021-01-26 18:14:31 -0700

    Added the second activity for the display screen

---
+++ app/src/main/java/com/example/madlibshallpass/DisplayHallPass.java
@@ -0,0 +1,14 @@
+package com.example.madlibshallpass;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DisplayHallPass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_hall_pass);
+    }
+}
